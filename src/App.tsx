57f67dfb536ee0cda624b297a5224d4360653c0c
Wilhelm Sterner <wilhelm.sterner@outlook.com>
2026-04-15 11:09:20 +0200

    Skapat dynamisk navigering genom react router samt skapat filsystem för stats

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,26 @@
 import "./App.css";
-import Calender from "./components/Calender/Calender";
+import Calender from "./pages/calender/Calender";
+import Stats from "./pages/stats/Stats";
 import NavigationMenu from "./components/Navigation/NavigtionMenu";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+//https://www.w3schools.com/react/react_router.asp
 
 function App() {
   return (
-    <div className="app-layout">
-      <NavigationMenu />
-      <main>
-        <Calender />
-      </main>
-    </div>
+    <BrowserRouter>
+      <div className="app-layout">
+        <NavigationMenu />
+
+        <main>
+          <Routes>
+            <Route path="/" element={<Calender />} />
+            <Route path="/calendar" element={<Calender />} />
+            <Route path="/stats" element={<Stats />} />
+          </Routes>
+        </main>
+      </div>
+    </BrowserRouter>
   );
 }
 
--- src/components/Navigation/NavigtionMenu.tsx
+++ src/components/Navigation/NavigtionMenu.tsx
@@ -1,15 +1,39 @@
 import "./navigation.css";
+import { NavLink } from "react-router-dom";
+
 export default function NavigationMenu() {
   return (
-    <>
-      <nav className="navigation-bar">
-        <div className="nav-items">
-          <a className="nav-item selected">Kalender</a>
-          <a className="nav-item">Statistik</a>
-          <a className="nav-item">Konto</a>
-        </div>
-        <div>&copy; Elit skier training log</div>
-      </nav>
-    </>
+    <nav className="navigation-bar">
+      <div className="nav-items">
+        <NavLink
+          to="/calendar"
+          className={({ isActive }) =>
+            isActive ? "nav-item selected" : "nav-item"
+          }
+        >
+          Kalender
+        </NavLink>
+
+        <NavLink
+          to="/stats"
+          className={({ isActive }) =>
+            isActive ? "nav-item selected" : "nav-item"
+          }
+        >
+          Statistik
+        </NavLink>
+
+        <NavLink
+          to="/account"
+          className={({ isActive }) =>
+            isActive ? "nav-item selected" : "nav-item"
+          }
+        >
+          Konto
+        </NavLink>
+      </div>
+
+      <div>&copy; Elit skier training log</div>
+    </nav>
   );
 }
